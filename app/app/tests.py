41d995711dcfbf2be60e36cf372025b68b775115
ChandrashekharRobbi <chandrashekarrobbi789@gmail.com>
2024-07-22 09:16:57 +0530

    Added POstgresql connection

---
+++ app/app/tests.py
@@ -0,0 +1,12 @@
+from django.test import SimpleTestCase
+
+from app import calc
+
+class CalcTests(SimpleTestCase):
+    """
+    Function to test calc.py
+    """
+    def test_add_numbers(self):
+        res = calc.add(5,6)
+        
+        self.assertEqual(res, 13)
