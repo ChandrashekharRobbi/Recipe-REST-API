a51c1e86801dd38e86ef16baee41b86f663187fb
ChandrashekharRobbi <chandrashekarrobbi789@gmail.com>
2024-07-22 09:21:14 +0530

    FIxed Linting

--- app/app/tests.py
+++ app/app/tests.py
@@ -2,11 +2,11 @@ from django.test import SimpleTestCase
 
 from app import calc
 
+
 class CalcTests(SimpleTestCase):
     """
     Function to test calc.py
     """
     def test_add_numbers(self):
-        res = calc.add(5,6)
-        
+        res = calc.add(5, 6)
         self.assertEqual(res, 11)
