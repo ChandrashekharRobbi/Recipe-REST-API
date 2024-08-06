e6a7010ad74e97589d45e850947b1c361a60aff7
ChandrashekharRobbi <chandrashekarrobbi789@gmail.com>
2024-08-06 23:49:42 +0530

    changes

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -1,16 +1,17 @@
 """
-Serializers for user app views
+Serializers for the user API View.
 """
-
 from django.contrib.auth import (
     get_user_model,
     authenticate,
 )
-from django.utils.translation import  gettext as _
+from django.utils.translation import gettext as _
+
 from rest_framework import serializers
 
+
 class UserSerializer(serializers.ModelSerializer):
-    """Serializer for the user object"""
+    """Serializer for the user object."""
 
     class Meta:
         model = get_user_model()
@@ -18,22 +19,12 @@ class UserSerializer(serializers.ModelSerializer):
         extra_kwargs = {'password': {'write_only': True, 'min_length': 5}}
 
     def create(self, validated_data):
-        """Create a new user with encrypted password and return it"""
+        """Create and return a user with encrypted password."""
         return get_user_model().objects.create_user(**validated_data)
 
-    def update(self, instance, validated_data):
-        """Update a user, setting the password correctly and return it"""
-        password = validated_data.pop('password', None)
-        user = super().update(instance, validated_data)
-
-        if password:
-            user.set_password(password)
-            user.save()
-
-        return user
 
 class AuthTokenSerializer(serializers.Serializer):
-    """Serializer for the user authentication object"""
+    """Serializer for the user auth token."""
     email = serializers.EmailField()
     password = serializers.CharField(
         style={'input_type': 'password'},
@@ -41,6 +32,7 @@ class AuthTokenSerializer(serializers.Serializer):
     )
 
     def validate(self, attrs):
+        """Validate and authenticate the user."""
         email = attrs.get('email')
         password = attrs.get('password')
         user = authenticate(
@@ -49,7 +41,7 @@ class AuthTokenSerializer(serializers.Serializer):
             password=password,
         )
         if not user:
-            msg = _('Unable to authenticate with provided credentials')
+            msg = _('Unable to authenticate with provided credentials.')
             raise serializers.ValidationError(msg, code='authorization')
 
         attrs['user'] = user
